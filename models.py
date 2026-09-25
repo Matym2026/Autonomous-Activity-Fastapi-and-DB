b51305fc350789615a35da715779a09f35374385
Matym2026 <hamoranma@uide.edu.ec>
2026-09-25 06:21:30 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 from sqlmodel import SQLModel, Field
 from datetime import date
+from typing import Optional
 
 class Usuario(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str
     email: str
 
 class Reserva(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     usuario_id: int = Field(foreign_key="usuario.id")
     fecha: date
     descripcion: str
